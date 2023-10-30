4f06898bfb70c78512feef906f2825568762cf85
Andreyfaf <147048858+Andreyfaf@users.noreply.github.com>
2023-10-30 13:47:28 +0300

    Add files via upload

---
+++ examtask1Andreyfaf/index.js
@@ -0,0 +1,21 @@
+function getRemainder = (a, b) =>{
+    const result = [a - b];
+    return result;
+    console.log(result); 
+};
+function calcPercentage = (a1, b1)
+ const result = [a1 / b1];
+ return result;
+ console.log(result);  
+function getLongestString(a10, b11) =>{
+    const getLongestString[house, streets];
+    console.log(getLongestString);
+};
+function concatStrings(a, b, i) =>{
+    const friend = ('friends','0','4');
+    (friends[4]);
+    console.log(concatStrings);
+};
+function normalizeVowels('OvErRun') =>{
+console.log('OvEr/Run');
+};
